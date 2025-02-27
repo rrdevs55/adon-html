f318347b07ba690f4c481a06bd3f4a6994b4aa1e
Md Jihad Hossan <jihad.marine@gmail.com>
2025-02-27 16:13:30 +0600

    preloder done

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,7 +123,7 @@
     }, 500);
 
     setTimeout(function () {
-      $('#preloader').fadeOut(1000, function () {
+      $('.loader-wrap').fadeOut(1000, function () {
         $(this).remove();
       });
     }, 3000);
@@ -138,6 +138,48 @@
     });
   });
 
+
+
+  const svg = document.getElementById("svg");
+  const tl = gsap.timeline();
+  const curve = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
+  const flat = "M0 2S175 1 500 1s500 1 500 1V0H0Z";
+
+  tl.to(".loader-wrap-heading .load-text , .loader-wrap-heading .cont", {
+    delay: 1.5,
+    y: -100,
+    opacity: 0,
+  });
+  tl.to(svg, {
+    duration: 0.5,
+    attr: {
+      d: curve
+    },
+    ease: "power2.easeIn",
+  }).to(svg, {
+    duration: 0.5,
+    attr: {
+      d: flat
+    },
+    ease: "power2.easeOut",
+  });
+  tl.to(".loader-wrap", {
+    y: -1500,
+  });
+  tl.to(".loader-wrap", {
+    zIndex: -1,
+    display: "none",
+  });
+  tl.from(
+    "main", {
+    y: 100,
+    opacity: 0,
+    delay: 0.3,
+  },
+    "-=1.5"
+  );
+
+  // Preloader end
 
   // Side Info Js
   $(".side-info-close,.offcanvas-overlay").on("click", function () {
