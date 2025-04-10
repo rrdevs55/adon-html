5b671977a960a1e2a384c8fe4181949d61adf593
Sarowar-Jahan <mdsarowarjahan3@gmail.com>
2025-04-10 16:08:10 +0600

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -751,6 +751,22 @@
     });
   }
 
+  // gsap nav 
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1); // Get section ID
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: 'smooth',
+        });
+      }
+    });
+  });
+
 
 })(jQuery);
 
