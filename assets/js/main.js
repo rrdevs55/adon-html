e28f353bbb04c54838351dc5b4667886147b1e6f
Sarowar-Jahan <mdsarowarjahan3@gmail.com>
2025-05-04 12:35:48 +0600

    checking testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -508,65 +508,19 @@
   }
 
 
-  // if ($('.work-area-2').length > 0 && window.innerWidth > 768) {
-  //   let work_area_2 = gsap.timeline({
-  //     scrollTrigger: {
-  //       trigger: ".work-area-2",
-  //       start: "top -20%",
-  //       end: "bottom 60%",
-  //       pin: ".section-header",
-  //       markers: false,
-  //       pinSpacing: false,
-  //       scrub: 3,
-  //     },
-  //   });
-  // }
 
-  if ($('.works-wrapper-box').length > 0 && window.innerWidth > 768) {
-    // let initialPosition = gsap.getProperty('.section-title', 'x'); // Capture the initial position
+
+  if ($('.works-wrapper-box-2').length > 0 && window.innerWidth > 768) {
 
     let work_area_2 = gsap.timeline({
       scrollTrigger: {
-        trigger: ".works-wrapper-box",
+        trigger: ".works-wrapper-box-2",
         start: "top 0%s", // Adjust this value as needed
         end: "bottom 70%", // Adjust this value as needed
         pin: ".section-header",
         markers: false,
         pinSpacing: false,
         scrub: 3,
-
-        // onEnterBack: () => {
-        //   // When section-title scrolls back up, show .remove-text class again immediately
-        //   gsap.to('.section-title .remove-text', {
-        //     opacity: 1,
-        //     visibility: 'visible',
-        //     x: '0',
-        //     duration: 1
-        //   });
-
-        //   // Move section-title back to its initial position (stored in initialPosition)
-        //   gsap.to('.section-title', {
-        //     x: initialPosition,
-        //     duration: 1
-        //   });
-        // },
-
-        // onLeave: () => {
-        //   // When section-title reaches the end position, hide .remove-text class first
-        //   gsap.to('.section-title .remove-text', {
-        //     opacity: 0,
-        //     visibility: 'hidden',
-        //     x: '-30',
-        //     duration: 1
-        //   });
-
-        //   // Then, move .section-title 200px to the right after the text is hidden
-        //   gsap.to('.work-area-2 .section-title', {
-        //     x: 200,
-        //     duration: 1
-        //   });
-        // },
-
       },
     });
   }
